6e0606bb1f0929f92675c2393402aacb5d03c145
aditya-2k23 <holaaditya123@gmail.com>
2025-03-14 16:09:18 +0530

    Update header margin for All Movies section in App component

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ const App = () => {
         </header>
 
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px]">All Movies</h2>
 
           {isLoading ? (
             <p className="text-white">
