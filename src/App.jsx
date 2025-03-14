ff778e90c373d577ed6ca3173d42fa2c5352835d
aditya-2k23 <holaaditya123@gmail.com>
2025-03-14 16:04:36 +0530

    Enhance loading state handling and display movie titles in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,19 @@ const App = () => {
         <section className="all-movies">
           <h2>All Movies</h2>
 
-          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          {isLoading ? (
+            <p className="text-white">Loading...</p>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p key={movie.id} className="text-white">
+                  {movie.title}
+                </p>
+              ))}
+            </ul>
+          )}
         </section>
 
         <h1 className="text-white">{searchTerm}</h1>
